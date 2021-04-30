5a81a0b77c764c6955d180406c8fc13975a5f559
Alex Pineda <h0h04dt@homeoffice.wal-mart.com>
2021-04-29 20:23:07 -0500

    main y pruebas unitarias

---
+++ main.py
@@ -0,0 +1,14 @@
+def suma(x, y):
+    if (type(x) == int and type(y)== int):
+        return x + y
+    else:
+        raise TypeError
+
+
+def resta(x, y):
+    return x - y
+
+
+cadena = """a;sdljkha;lkdsjf;lkajsd;lkn;lakns;
+            ldknv;lakns;dlknf;lkasnd;lkfna;
+            lksdn;lkvna;lksndvf"""
---
+++ test_main.py
@@ -0,0 +1,14 @@
+import pytest
+from main import suma, resta
+
+def test_suma():
+    assert suma(1,1) == 2
+    assert suma(1,2) == 3
+
+def test_suma_exception():
+    with pytest.raises(TypeError):
+        suma(1, 1)
+
+def test_resta():
+    assert resta(1,1) == 0
+    assert resta(1,2) == -1
